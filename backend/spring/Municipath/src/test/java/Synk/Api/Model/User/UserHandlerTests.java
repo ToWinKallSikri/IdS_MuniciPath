de1f48626401aeb5111600de78c22ea6ccac7763
ToWinKallSikri <116014231+ToWinKallSikri@users.noreply.github.com>
2024-01-03 15:48:24 +0100

    [update]
    User handler tests

--- backend/spring/Municipath/src/test/java/Synk/Api/Model/User/UserHandlerTests.java
+++ backend/spring/Municipath/src/test/java/Synk/Api/Model/User/UserHandlerTests.java
@@ -3,16 +3,48 @@ package Synk.Api.Model.User;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 
 @SpringBootTest
 public class UserHandlerTests {
+
+	@Autowired
+	private UserHandler uh;
 	
 	@Test
 	void testInitialization() {
 		assertDoesNotThrow(() -> new UserHandler());
 	}
+
+	@Test
+	void testAddRemoveUser () {
+		assertTrue(uh.addUser("naruto", "password"));
+		uh.userValidation("naruto");
+		assertFalse(uh.addUser("naruto", "newPassword"));
+		assertTrue(uh.removeUser("naruto"));
+		uh.removeUser("naruto");
+	}
+
+	@Test
+	void testChangePassword() {
+		uh.addUser("naruto", "password");
+		uh.userValidation("naruto");
+		assertTrue(uh.changePassowrd("naruto", "newPassword"));
+		assertFalse(uh.changePassowrd("sasuke", "newPassword"));
+		uh.removeUser("naruto");
+	}
+
+	@Test
+	void testUserValidation() {
+		uh.addUser("naruto", "password");
+		assertTrue(uh.userValidation("naruto"));
+		assertFalse(uh.userValidation("sasuke"));
+		uh.removeUser("naruto");
+	}
+
+
 	
 }
