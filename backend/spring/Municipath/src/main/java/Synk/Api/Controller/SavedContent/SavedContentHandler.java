cda95d9bb525c0204e9aff7e6e7c356773596f01
twinfore <twinkal.sikri@studenti.unicam.it>
2024-01-16 17:34:45 +0100

    [update]
    SavedContentHandler

--- backend/spring/Municipath/src/main/java/Synk/Api/Controller/SavedContent/SavedContentHandler.java
+++ backend/spring/Municipath/src/main/java/Synk/Api/Controller/SavedContent/SavedContentHandler.java
@@ -1,23 +1,55 @@
 package Synk.Api.Controller.SavedContent;
 
+import Synk.Api.Controller.MuniciPathMediator;
+import Synk.Api.Model.SavedContent.SavedContent;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class SavedContentHandler {
 
-	public List<String> getPartecipants(String contentId){
-		return null;
-	}
-	
+    private MuniciPathMediator mediator;
+
+    private List<SavedContent> savedContents;
+
+    public void setMediator(MuniciPathMediator mediator) {
+        this.mediator = mediator;
+    }
+
+    public SavedContentHandler() {
+        this.savedContents = new ArrayList<>();
+    }
+
+    public List<String> getPartecipants(String contentId){
+        return savedContents.stream().
+                filter(s -> s.getContentId().equals(contentId))
+                .map(SavedContent::getUsername).toList();
+    }
+
 	public boolean saveSavedContent(String username, String contentId) {
-		return false;
+        if (!(mediator.usernameExists(username) && mediator.contentExist(contentId)))
+                return false;
+        SavedContent s1 = new SavedContent(username, contentId);
+        if(savedContents.stream().anyMatch(s -> s.getId().equals(s1.getId())))
+            return false;
+        return savedContents.add(s1);
 	}
-	
+
 	public boolean removeSavedContent(String username, String contentId) {
-		return false;
-	} 
-	
-	public List<String> getSavedContent(String username){
-		return null;
+        if (!(mediator.usernameExists(username) && mediator.contentExist(contentId)))
+            return false;
+        SavedContent s1 = new SavedContent(username, contentId);
+        if(!(savedContents.stream().anyMatch(s -> s.getId().equals(s1.getId()))))
+            return false;
+        return savedContents.remove(s1);
+	}
+
+	public List<String> getSavedContent(String username) {
+        if(!(mediator.usernameExists(username)))
+            return null;
+		return savedContents.stream()
+                .filter(s -> s.getUsername().equals(username))
+                .map(SavedContent::getContentId).toList();
 	}
 	
 }
