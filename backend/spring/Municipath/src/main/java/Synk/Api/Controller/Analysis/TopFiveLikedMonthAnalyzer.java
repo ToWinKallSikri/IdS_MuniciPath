a2c7215028bf8e50bbd74c6f4fbb1cf552cd23ee
twinfore <twinkal.sikri@studenti.unicam.it>
2024-01-19 17:10:07 +0100

    [update]
    Top Five Liked Contents in the last n months analyzer

--- backend/spring/Municipath/src/main/java/Synk/Api/Controller/Analysis/TopFiveLikedMonthAnalyzer.java
+++ backend/spring/Municipath/src/main/java/Synk/Api/Controller/Analysis/TopFiveLikedMonthAnalyzer.java
@@ -1,25 +1,56 @@
 package Synk.Api.Controller.Analysis;
 
+import Synk.Api.Model.Feedback.Score;
 import Synk.Api.Model.MetaData;
 
 public class TopFiveLikedMonthAnalyzer implements TopFiveMonthAnalyzer {
 
+    private String[] top5;
+    private double [] topLiked;
+    private final double Z = 1.96f;
 	@Override
 	public void StartAnalysis() {
-		// TODO Auto-generated method stub
-		
+		top5 = new String[5];
+		topLiked = new double[5];
 	}
 
 	@Override
 	public void acceptData(MetaData data) {
-		// TODO Auto-generated method stub
-		
-	}
+        double wScore = wilsonScore(data.getVote());
+        if ( wScore > this.topLiked[4]) {
+            this.topLiked[4] = wScore;
+            this.top5[4] = data.getId();
+            insertIntoTop(4);
+        }
+    }
 
-	@Override
-	public String[] getResult() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        @Override
+        public String[] getResult () {
+            return top5;
+        }
+
+        private double wilsonScore (Score vote) {
+            double p = vote.getVoteAverage() / 5;
+            double n = vote.getVoteCount();
+            double left = p + ((Z * Z) / (2 * n));
+            double right = Z * Math.sqrt((p * (1 - p) / n) + ((Z * Z) / (4 * n * n)));
+            double under = 1 + ((Z * Z) / n);
+            return (left - right) / under;
+        }
+
+        private void insertIntoTop ( int index){
+            if (index < 1)
+                return;
+            if (this.topLiked[index] > this.topLiked[index - 1]) {
+                double likeTemp = this.topLiked[index - 1];
+                String idTemp = this.top5[index - 1];
+                this.topLiked[index - 1] = this.topLiked[index];
+                this.top5[index - 1] = this.top5[index];
+                this.topLiked[index] = likeTemp;
+                this.top5[index] = idTemp;
+                insertIntoTop(index - 1);
+            }
+        }
+
 
 }
