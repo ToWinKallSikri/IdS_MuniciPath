9a1b8e1fda3cf8f433b92ff3a910bb1c123410ca
twinfore <twinkal.sikri@studenti.unicam.it>
2024-01-19 18:00:26 +0100

    [update]

---
+++ backend/spring/Municipath/src/main/java/Synk/Api/View/RestLogController.java
@@ -0,0 +1,49 @@
+package Synk.Api.View;
+
+import Synk.Api.Controller.User.UserHandler;
+import Synk.Api.View.Auth.Authenticator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestLogController {
+    @Autowired
+    private UserHandler uh;
+    private Authenticator authenticator;
+    private WebResponseCreator wrc;
+
+    @PostMapping(value="/api/v1/signin")
+    public ResponseEntity<Object> signin(@RequestParam("username") String username, @RequestParam("password") String password) {
+        if(uh.addUser(username, password)) {
+            return new ResponseEntity<Object>(wrc.make("Creazione account riuscito."), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping(value="/api/v1/changePassword")
+    public ResponseEntity<Object> changePassword(@RequestHeader(name="auth") String token,
+                                                 @RequestParam("newPassword") String newPassword) {
+        String username = authenticator.getUsername(token);
+        if(this.uh.changePassword(username, newPassword)) {
+            return new ResponseEntity<Object>(wrc.make("Password cambiata."), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+
+    @GetMapping(value="/api/v1/login")
+    public ResponseEntity<Object> login(@RequestParam("username") String username,
+                                        @RequestParam("password") String password) {
+        if(this.uh.isThePassword(username, password)) {
+            String jwt = authenticator.createJwt(username);
+            return new ResponseEntity<Object>(wrc.make(jwt), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
---
+++ backend/spring/Municipath/src/main/java/Synk/Api/View/RestManagerController.java
@@ -0,0 +1,141 @@
+package Synk.Api.View;
+
+import Synk.Api.Controller.City.CityHandler;
+import Synk.Api.Controller.User.UserHandler;
+import Synk.Api.Model.User.User;
+import Synk.Api.View.Auth.Authenticator;
+import Synk.Api.View.Auth.Authorizer;
+import Synk.Api.View.ViewModel.ProtoCity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestManagerController {
+    @Autowired
+    private UserHandler uh;
+    @Autowired
+    private CityHandler ch;
+    @Autowired
+    private Authorizer authorizer;
+    private Authenticator authenticator;
+    private WebResponseCreator wrc;
+
+
+    @DeleteMapping(value="/api/v1/manager/removeUser")
+    public ResponseEntity<Object> removeUser(@RequestHeader(name="auth") String token,
+                                             @RequestParam("toRemove") String toRemove) {
+        String username = authenticator.getUsername(token);
+        if(this.authorizer.isManager(username) &&  uh.removeUser(toRemove)) {
+            return new ResponseEntity<Object>(wrc.make("Utente rimosso."), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    @PutMapping(value="/api/v1/manager/convalidate")
+    public ResponseEntity<Object> convalidateAccount(@RequestHeader(name="auth") String token, @RequestParam("toValidate") String toValidate){
+        String username = authenticator.getUsername(token);
+        if(this.authorizer.isManager(username) && uh.userValidation( toValidate)) {
+            return new ResponseEntity<Object>(wrc.make("Account convalidato."), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+
+    @GetMapping(value="/api/v1/manager/getUsersNotConvalidated")
+    public ResponseEntity<Object> getUsersNotConvalidated(@RequestHeader(name="auth") String token) {
+        String username = authenticator.getUsername(token);
+        if(this.authorizer.isManager(username)) {
+            List<User> list = this.uh.getNotConvalidatedUsers();
+            if (list != null) {
+                return new ResponseEntity<Object>(list, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
+            }
+        }
+        return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+    }
+
+
+
+    @PutMapping(value="/api/v1/manager/addManager")
+    public ResponseEntity<Object> addManager(@RequestHeader(name="auth") String token,
+                                             @RequestParam("toManage") String toManage) {
+        String username = authenticator.getUsername(token);
+        if(this.authorizer.isManager(username)) {
+            if (this.uh.manageManager(toManage, true)) {
+                return new ResponseEntity<Object>(wrc.make("Manager aggiunto."), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+            }
+        }
+        return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+    }
+
+
+
+    @PutMapping(value="/api/v1/manager/removeManager")
+    public ResponseEntity<Object> removeManager(@RequestHeader(name="auth") String token,
+                                                @RequestParam("toManage") String toManage) {
+        String username = authenticator.getUsername(token);
+        if(this.authorizer.isManager(username)) {
+            if (this.uh.manageManager(toManage, false)) {
+                return new ResponseEntity<Object>(wrc.make("Manager rimosso."), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+            }
+        }
+        return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+    }
+
+
+    @PostMapping(value="/api/v1/manager/createCity")
+    public ResponseEntity<Object> createCity(@RequestHeader(name="auth") String token,
+                                             @RequestBody ProtoCity pc) {
+        String username = authenticator.getUsername(token);
+        if(this.authorizer.isManager(username)) {
+            if (this.ch.createCity(pc.getCityName(), pc.getCap(), pc.getCurator(), pc.getPos())) {
+                return new ResponseEntity<Object>(wrc.make("Comune " + pc.getCityName() + " creato."), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+            }
+        }
+        return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+    }
+
+    @PutMapping(value="/api/v1/manager/updateCity")
+    public ResponseEntity<Object> updateCity(@RequestHeader(name="auth") String token,
+                                             @RequestParam("cityId") String cityId,
+                                             @RequestBody ProtoCity pc) {
+        String username = authenticator.getUsername(token);
+        if(this.authorizer.isManager(username)) {
+            if (this.ch.updateCity(cityId, pc.getCityName(), pc.getCap(), pc.getCurator(), pc.getPos())) {
+                return new ResponseEntity<Object>(wrc.make("Comune aggiornato."), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+            }
+        }
+        return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping(value="/api/v1/manager/deleteCity")
+    public ResponseEntity<Object> deleteCity(@RequestHeader(name="auth") String token,
+                                             @RequestParam("cityId") String cityId) {
+        String username = authenticator.getUsername(token);
+        if (this.authorizer.isManager(username)) {
+            if (this.ch.deleteCity(cityId)) {
+                return new ResponseEntity<Object>(wrc.make("Comune eliminato."), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+            }
+        }
+        return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+    }
+}
