7abfc02f9666f866a11e40393b858a4088083b3a
Marvin <marvin.sincini@studenti.unicam.it>
2024-01-25 17:06:36 +0100

    [commit veloce]
    Co-Authored-By: twinfore <116014231+ToWinKallSikri@users.noreply.github.com>

--- frontend/angular/Municipath/src/app/point.service.ts
+++ frontend/angular/Municipath/src/app/point.service.ts
@@ -1,13 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams  } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders  } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Point } from './Point';
+import { Post } from './Post';
+import { environment } from '../environments/environment.development';
+import { catchError } from 'rxjs/operators';
+import { SharedService } from './shared.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PointService {
 
-  constructor(private HttpClient: HttpClient) { 
+  constructor(private HttpClient: HttpClient, private cookieService : SharedService) {}
 
-    
+  public getPoints(cityId : string) : Observable<Point[]> {
+    let header = new HttpHeaders().append('auth', this.cookieService.get('jwt'));
+    return this.HttpClient.get<Point[]>(environment.baseUrl+'/api/v1/city/'+cityId+'/points', { headers: header });
   }
+
+  public getPoint(cityId : string, pointId : string) : Observable<Point> {
+    let header = new HttpHeaders().append('auth', this.cookieService.get('jwt'));
+    let param = new HttpParams().append('pointId', pointId);
+    return this.HttpClient.get<Point>(environment.baseUrl+'/api/v1/city/'+cityId+'/point', { headers: header, params: param });
+  }
+
+  public getPost(cityId : string, postId : string) : Observable<Post> {
+    let header = new HttpHeaders().append('auth', this.cookieService.get('jwt'));
+    let param = new HttpParams().append('postId', postId);
+    return this.HttpClient.get<Post>(environment.baseUrl+'/api/v1/city/'+cityId+'/post', { headers: header, params: param });
+  }
+  
 }
